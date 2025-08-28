81fc12cb8ee33a777bbc6ecb8ca1665c79b48d3c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-28 09:27:55 -0300

    add router-outlet to user layout component

--- src/app/admin/user/user-layout/user-layout.component.ts
+++ src/app/admin/user/user-layout/user-layout.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-layout',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.scss'
 })
