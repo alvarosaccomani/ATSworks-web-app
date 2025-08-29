0913db0f5ac298837feee6dd69a82ea611db3026
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-29 20:52:37 -0300

    i add the customer/:cus_uuid route in user.routes.ts

--- src/app/admin/user/user.routes.ts
+++ src/app/admin/user/user.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomersComponent } from './customers/customers.component';
+import { CustomerComponent } from './customer/customer.component';
 
 export const USER_ROUTES: Routes = [
     {
@@ -10,7 +11,8 @@ export const USER_ROUTES: Routes = [
         component: UserLayoutComponent,
         children: [
             { path: 'dashboard', component: DashboardComponent},
-            { path: 'customers', component: CustomersComponent}
+            { path: 'customers', component: CustomersComponent},
+            { path: 'customer/:cus_uuid', component: CustomerComponent}
         ]
     }
 ];
