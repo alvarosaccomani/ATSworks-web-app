c964dac4b641c8f6290751f8db14ca1de406e348
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-28 23:10:49 -0300

    i add side-bar component into user-layout component

--- src/app/admin/user/user-layout/user-layout.component.ts
+++ src/app/admin/user/user-layout/user-layout.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SideBarComponent } from '../../../shared/components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-user-layout',
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SideBarComponent
   ],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.scss'
