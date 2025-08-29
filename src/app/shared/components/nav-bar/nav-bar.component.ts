43eb29aa2f64a5def04d0683ac099f89392271e5
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-29 00:10:04 -0300

    i add exit button in nav-bar component

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 declare var $:any;
+declare var Swal: any;
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,6 +11,10 @@ declare var $:any;
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent implements OnInit {
+
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
     /*  Show/Hidden Nav Lateral */
@@ -23,6 +29,25 @@ export class NavBarComponent implements OnInit {
         NavLateral.addClass('active');
         PageConten.addClass('active');
       }
+    });
+
+    /*  Exit system buttom */
+    $('.btn-exit-system').on('click', (e: any) => {
+      e.preventDefault();
+      Swal.fire({
+        title: 'Are you sure to close the session?',
+        text: "You are about to close the session and exit the system",
+        type: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, exit!',
+        cancelButtonText: 'No, cancel'
+      }).then((result: any) => {
+        if (result.value) {
+          this._router.navigate(['/auth/login'])
+        }
+      });
     });
   }
 
