baad07789abca56e8e005366ed9c3583f8ae525d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-28 22:12:29 -0300

    i add nav-bar component into dashboard component

--- src/app/admin/user/dashboard/dashboard.component.ts
+++ src/app/admin/user/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    NavBarComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
