fb62a6c5a8a6d96c2e5c283eb432412a26603167
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-29 20:53:59 -0300

    i add the /admin/user/customer/new route in customers component

--- src/app/admin/user/customers/customers.component.ts
+++ src/app/admin/user/customers/customers.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-customers',
   imports: [
+    RouterLink,
     NavBarComponent
   ],
   templateUrl: './customers.component.html',
