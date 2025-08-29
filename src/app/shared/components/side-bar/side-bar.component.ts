291b650bb6d60d0607050accd9acb02ac7237f3f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-28 23:33:21 -0300

    i add url property in side-bar component

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
@@ -15,91 +17,109 @@ export class SideBarComponent {
     {
       name: 'Dashboard',
       icon: 'fab fa-dashcube fa-fw',
-      hasSubmenu: false
+      hasSubmenu: false,
+      url: 'dashboard'
     },
     {
       name: 'Clientes',
       icon: 'fas fa-users fa-fw',
+      url: null,
       hasSubmenu: true,
       isOpen: false,
       submenu: [
         { 
           name: 'Agregar Cliente',
-          icon: 'fas fa-plus fa-fw'
+          icon: 'fas fa-plus fa-fw',
+          url: null
         },
         { 
           name: 'Lista de clientes',
-          icon: 'fas fa-clipboard-list fa-fw'
+          icon: 'fas fa-clipboard-list fa-fw',
+          url: 'customers'
         },
         {
           name: 'Buscar cliente',
-          icon: 'fas fa-search fa-fw'
+          icon: 'fas fa-search fa-fw',
+          url: null
         }
       ]
     },
     {
       name: 'Items',
       icon: 'fas fa-pallet fa-fw',
+      url: null,
       hasSubmenu: true,
       isOpen: false,
       submenu: [
         { 
           name: 'Agregar item',
-          icon: 'fas fa-plus fa-fw'
+          icon: 'fas fa-plus fa-fw',
+          url: null
         },
         { 
           name: 'Lista de items',
-          icon: 'fas fa-clipboard-list fa-fw'
+          icon: 'fas fa-clipboard-list fa-fw',
+          url: null
         },
         {
           name: 'Buscar item',
-          icon: 'fas fa-search fa-fw'
+          icon: 'fas fa-search fa-fw',
+          url: null
         }
       ]
     },
     {
       name: 'Préstamos',
       icon: 'fas fa-file-invoice-dollar fa-fw',
+      url: null,
       hasSubmenu: true,
       isOpen: false,
       submenu: [
         { 
           name: 'Nuevo préstamo',
-          icon: 'fas fa-plus fa-fw'
+          icon: 'fas fa-plus fa-fw',
+          url: null
         },
         { 
           name: 'Lista de préstamos',
-          icon: 'fas fa-clipboard-list fa-fw'
+          icon: 'fas fa-clipboard-list fa-fw',
+          url: null
         },
         {
           name: 'Buscar préstamos',
-          icon: 'fas fa-search fa-fw'
+          icon: 'fas fa-search fa-fw',
+          url: null
         }
       ]
     },
     {
       name: 'Usuarios',
       icon: 'fas fa-user-secret fa-fw',
+      url: null,
       hasSubmenu: true,
       isOpen: false,
       submenu: [
         { 
           name: 'Nuevo usuario',
-          icon: 'fas fa-plus fa-fw'
+          icon: 'fas fa-plus fa-fw',
+          url: null
         },
         { 
           name: 'Lista de usuarios',
-          icon: 'fas fa-clipboard-list fa-fw'
+          icon: 'fas fa-clipboard-list fa-fw',
+          url: null
         },
         {
           name: 'Buscar usuarios',
-          icon: 'fas fa-search fa-fw'
+          icon: 'fas fa-search fa-fw',
+          url: null
         }
       ]
     },
     {
       name: 'Empresa',
       icon: 'fas fa-store-alt fa-fw',
+      url: null,
       hasSubmenu: false
     }
   ];
