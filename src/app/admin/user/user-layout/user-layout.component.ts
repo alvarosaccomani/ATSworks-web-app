c7ed21330011198497c2c6d640551dc44b3a24cb
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-30 18:25:46 -0300

    add nav-bar component into user-layout component

--- src/app/admin/user/user-layout/user-layout.component.ts
+++ src/app/admin/user/user-layout/user-layout.component.ts
@@ -2,17 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SideBarComponent } from '../../../shared/components/side-bar/side-bar.component';
+import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
+import { UserInterface } from '../../../core/interfaces/user.interface';
+import { UsersService } from '../../../core/services/users.service';
 
 @Component({
   selector: 'app-user-layout',
   imports: [
     CommonModule,
     RouterModule,
-    SideBarComponent
+    SideBarComponent,
+    NavBarComponent
   ],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.scss'
 })
 export class UserLayoutComponent {
+
+  public identity!: UserInterface;
+
+  constructor(
+    private _usersService: UsersService
+  ) { }
+
+  ngOnInit(): void {
+    this.identity = this._usersService.getIdentity();
+  }
 
 }
