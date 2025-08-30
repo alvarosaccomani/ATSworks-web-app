0d9508063f659c2d9b5ef4afde5a16384b963701
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-30 18:17:20 -0300

    i remove nav-bar component from dashboard component

--- src/app/admin/user/dashboard/dashboard.component.ts
+++ src/app/admin/user/dashboard/dashboard.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [
-    NavBarComponent
-  ],
+  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
