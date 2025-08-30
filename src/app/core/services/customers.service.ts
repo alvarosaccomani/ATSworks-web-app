7ca2b5f108623495f1fbfa534a7fa9dc2873452c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-29 22:51:15 -0300

    add method getCustomers to customers service

--- src/app/core/services/customers.service.ts
+++ src/app/core/services/customers.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
+import { CustomerResults  } from '../interfaces/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+
+  public getCustomers(filter: string, page?: number, perPage?: number): Observable<CustomerResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
+    }
+
+    return this._http.get<CustomerResults>(this._GlobalService.url + 'customers/' + filter, {headers:headers})
+  }
 }
