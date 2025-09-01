d5adbda4439f3e7d9e5db3251128516d62aed818
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-31 22:49:56 -0300

    i add application-layout component to project

---
+++ src/app/admin/application/application-layout/application-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-layout',
+  imports: [],
+  templateUrl: './application-layout.component.html',
+  styleUrl: './application-layout.component.scss'
+})
+export class ApplicationLayoutComponent {
+
+}
