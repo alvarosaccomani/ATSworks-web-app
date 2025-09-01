8f0ac3f7bf21bb6705df5b5b03692c33e690cea8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-31 22:58:52 -0300

    i add side-bar and nav-bar component and router-outlet into application-layout component

--- src/app/admin/application/application-layout/application-layout.component.ts
+++ src/app/admin/application/application-layout/application-layout.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SideBarComponent } from '../../../shared/components/side-bar/side-bar.component';
+import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.component';
+import { UserInterface } from '../../../core/interfaces/user.interface';
+import { UsersService } from '../../../core/services/users.service';
 
 @Component({
   selector: 'app-application-layout',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SideBarComponent,
+    NavBarComponent
+  ],
   templateUrl: './application-layout.component.html',
   styleUrl: './application-layout.component.scss'
 })
 export class ApplicationLayoutComponent {
+
+  public identity!: UserInterface;
+
+  constructor(
+    private _usersService: UsersService
+  ) { }
+
+  ngOnInit(): void {
+    this.identity = this._usersService.getIdentity();
+  }
 
 }
