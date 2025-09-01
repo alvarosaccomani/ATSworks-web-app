873952940c27b4523090fb8eeceefc2ce7cc0b18
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-01 19:44:51 -0300

    i add the users route in application.routes.ts

--- src/app/admin/application/application.routes.ts
+++ src/app/admin/application/application.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
+import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
 
 export const APPLICATION_ROUTES: Routes = [
@@ -8,6 +9,7 @@ export const APPLICATION_ROUTES: Routes = [
         path: '',
         component: ApplicationLayoutComponent,
         children: [
+            { path: 'users', component: UsersComponent},
             { path: 'user/:usr_uuid', component: UserComponent}
         ]
     }
