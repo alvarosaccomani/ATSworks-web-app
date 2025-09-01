c88dd865d9901d8f6f25f1191a0fea1ddfe25f9a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-31 22:49:09 -0300

    i add application route to admin.routes.ts file in project

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const ADMIN_ROUTES: Routes = [
     {
+        path: 'application',
+        loadChildren: () => import('./application/application.routes').then(m => m.APPLICATION_ROUTES),
+    },
+    {
         path: 'user',
         loadChildren: () => import('./user/user.routes').then(m => m.USER_ROUTES),
     }
