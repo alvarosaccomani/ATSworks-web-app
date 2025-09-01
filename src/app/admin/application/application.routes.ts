d8aaad3f7710ec5aa676c4970bc4b9cc3d8f6f4b
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-31 23:05:20 -0300

    i add the user/:usr_uuid route in application.routes.ts

--- src/app/admin/application/application.routes.ts
+++ src/app/admin/application/application.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
+import { UserComponent } from './user/user.component';
 
 export const APPLICATION_ROUTES: Routes = [
     {
         path: '',
         component: ApplicationLayoutComponent,
-        children: []
+        children: [
+            { path: 'user/:usr_uuid', component: UserComponent}
+        ]
     }
 ];
