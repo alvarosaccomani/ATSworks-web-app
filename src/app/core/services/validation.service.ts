f4d2e20c33393651418669904fe55a90d8b85daa
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-01 19:02:31 -0300

    emailValidator

--- src/app/core/services/validation.service.ts
+++ src/app/core/services/validation.service.ts
@@ -6,4 +6,10 @@ import { Injectable } from '@angular/core';
 export class ValidationService {
 
   constructor() { }
+
+  public emailValidator(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValid = emailRegex.test(email);
+    return isValid;
+  }
 }
