97d5427741eaadaf05ff024e3898dba41f357fc0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-02 23:00:20 -0300

    inject _itemsService in items component

--- src/app/admin/application/items/items.component.ts
+++ src/app/admin/application/items/items.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ItemResults } from '../../../core/interfaces/item';
+import { ItemsService } from '../../../core/services/items.service';
 
 @Component({
   selector: 'app-items',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    RouterLink
+  ],
   templateUrl: './items.component.html',
   styleUrl: './items.component.scss'
 })
-export class ItemsComponent {
+export class ItemsComponent implements OnInit {
+
+  public items$!: Observable<ItemResults>;
+  
+    constructor(
+      private _itemsService: ItemsService
+    ) { }
+    
+    ngOnInit(): void {
+      this.items$ = this._itemsService.getItems('');
+    }
 
 }
