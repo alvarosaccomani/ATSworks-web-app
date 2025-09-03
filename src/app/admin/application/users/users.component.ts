7cd32ce4a5ec8550efd548935380c0602a372430
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-02 22:25:37 -0300

    inject _usersService in users component

--- src/app/admin/application/users/users.component.ts
+++ src/app/admin/application/users/users.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserResults } from '../../../core/interfaces/user';
+import { UsersService } from '../../../core/services/users.service';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    RouterLink
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+
+  public users$!: Observable<UserResults>;
+  
+    constructor(
+      private _usersService: UsersService
+    ) { }
+    
+    ngOnInit(): void {
+      this.users$ = this._usersService.getUsers('');
+    }
 
 }
