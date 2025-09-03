4001c60f4eae1716ef7b85a759903bb85de8ff47
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-02 22:37:58 -0300

    i add the items route in application.routes.ts

--- src/app/admin/application/application.routes.ts
+++ src/app/admin/application/application.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
+import { ItemsComponent } from './items/items.component';
 
 export const APPLICATION_ROUTES: Routes = [
     {
@@ -10,7 +11,8 @@ export const APPLICATION_ROUTES: Routes = [
         component: ApplicationLayoutComponent,
         children: [
             { path: 'users', component: UsersComponent},
-            { path: 'user/:usr_uuid', component: UserComponent}
+            { path: 'user/:usr_uuid', component: UserComponent},
+            { path: 'items', component: ItemsComponent}
         ]
     }
 ];
