e6da897da48394d0e50d1ca53c237ce708221559
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-03 17:31:11 -0300

    i add model-item component to project

---
+++ src/app/admin/user/model-item/model-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-item',
+  imports: [],
+  templateUrl: './model-item.component.html',
+  styleUrl: './model-item.component.scss'
+})
+export class ModelItemComponent {
+
+}
