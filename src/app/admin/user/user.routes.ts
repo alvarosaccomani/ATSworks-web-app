8efd8ecf5c81c54012c3aed9d30c2ff3777a8b9e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-03 08:34:21 -0300

    i add the models-items route in user.routes.ts

--- src/app/admin/user/user.routes.ts
+++ src/app/admin/user/user.routes.ts
@@ -4,6 +4,7 @@ import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomerComponent } from './customer/customer.component';
+import { ModelsItemsComponent } from './models-items/models-items.component';
 
 export const USER_ROUTES: Routes = [
     {
@@ -12,7 +13,8 @@ export const USER_ROUTES: Routes = [
         children: [
             { path: 'dashboard', component: DashboardComponent},
             { path: 'customers', component: CustomersComponent},
-            { path: 'customer/:cus_uuid', component: CustomerComponent}
+            { path: 'customer/:cus_uuid', component: CustomerComponent},
+            { path: 'models-items', component: ModelsItemsComponent}
         ]
     }
 ];
