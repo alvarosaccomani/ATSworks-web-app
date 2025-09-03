2bb4b916a2bb6565c2ef8e8efdaa9796d4c82a22
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-03 08:35:28 -0300

    inject _modelItemsService in models-items component

--- src/app/admin/user/models-items/models-items.component.ts
+++ src/app/admin/user/models-items/models-items.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ModelItemsService } from '../../../core/services/model-items.service';
+import { ModelItemResults } from '../../../core/interfaces/model-item';
+import { SharedDataService } from '../../../core/services/shared-data.service';
 
 @Component({
   selector: 'app-models-items',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    RouterLink
+  ],
   templateUrl: './models-items.component.html',
   styleUrl: './models-items.component.scss'
 })
 export class ModelsItemsComponent {
 
+  public modelItems$!: Observable<ModelItemResults>;
+
+  constructor(
+    private _modelItemsService: ModelItemsService,
+    private _sharedDataService: SharedDataService
+  ) { }
+  
+  ngOnInit(): void {
+    this.modelItems$ = this._modelItemsService.getModelItems('');
+    this._sharedDataService.selectedCompany$.subscribe((company) => {
+      if (company) {
+        console.info(company);
+      }
+    });
+  }
 }
