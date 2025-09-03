5f50822d5b181f1d9fc7aca4bbd726f09437fa4e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-02 22:59:15 -0300

    add method getItems to items service

--- src/app/core/services/items.service.ts
+++ src/app/core/services/items.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
+import { ItemResults } from '../interfaces/item/item-results.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+
+  public getItems(filter: string, page?: number, perPage?: number): Observable<ItemResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
+    }
+
+    return this._http.get<ItemResults>(this._GlobalService.url + 'items/' + filter, {headers:headers})
+  }
 }
