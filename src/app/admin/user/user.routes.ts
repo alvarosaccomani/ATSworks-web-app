9a91002a955d2c07719e893df21abdeb4fb52867
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-04 14:20:32 -0300

    i add the works route in user.routes.ts

--- src/app/admin/user/user.routes.ts
+++ src/app/admin/user/user.routes.ts
@@ -6,6 +6,7 @@ import { CustomersComponent } from './customers/customers.component';
 import { CustomerComponent } from './customer/customer.component';
 import { ModelsItemsComponent } from './models-items/models-items.component';
 import { ModelItemComponent } from './model-item/model-item.component';
+import { WorksComponent } from './works/works.component';
 import { CompanyProfileComponent } from './company-profile/company-profile.component';
 
 export const USER_ROUTES: Routes = [
@@ -18,6 +19,7 @@ export const USER_ROUTES: Routes = [
             { path: 'customer/:cus_uuid', component: CustomerComponent },
             { path: 'models-items', component: ModelsItemsComponent },
             { path: 'model-item/:itm_uuid/:cmpitm_uuid/:mitm_uuid', component: ModelItemComponent },
+            { path: 'works', component: WorksComponent },
             { path: 'company-profile/:cmp_uuid', component: CompanyProfileComponent }
         ]
     }
