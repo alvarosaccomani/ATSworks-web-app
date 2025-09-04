5bd49b86eee20c4cee1285b96c809e831c5d23b0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-04 08:54:46 -0300

    i add cmp_uuid parameter at getModelItems and saveModelItem method in model-items service

--- src/app/core/services/model-items.service.ts
+++ src/app/core/services/model-items.service.ts
@@ -14,19 +14,26 @@ export class ModelItemsService {
     private _GlobalService: GlobalService
   ) { }
 
-  public getModelItems(filter: string, page?: number, perPage?: number): Observable<ModelItemResults> {
+  public getModelItems(cmp_uuid: string, filter?: string, page?: number, perPage?: number): Observable<ModelItemResults> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
     if(page && perPage) {
       filter = `${filter}/${page}/${perPage}`;
     }
 
-    return this._http.get<ModelItemResults>(this._GlobalService.url + 'model-items/' + filter, {headers:headers})
+    return this._http.get<ModelItemResults>(this._GlobalService.url + 'model-items/' + cmp_uuid + '/' + filter, {headers:headers})
   }
 
   public getModelItemById(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Observable<any> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
     return this._http.get(this._GlobalService.url + 'model-item/' + cmp_uuid + '/' + itm_uuid + '/' + cmpitm_uuid + '/' + mitm_uuid, {headers:headers});
+  }
+
+  public saveModelItem(modelItem: any): Observable<any> {
+    let params = JSON.stringify(modelItem);
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.post(this._GlobalService.url + 'model-item', params, {headers:headers});
   }
 }
