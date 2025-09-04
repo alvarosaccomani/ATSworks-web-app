d7a3f3ae820f4801ea5f1e3bce3d3d396c3f0df9
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-04 14:28:35 -0300

    i add item Trabajos and route /admin/user/company-profile/cmp_uuid to side-bar component

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -18,161 +18,165 @@ export class SideBarComponent {
     private _sharedDataService: SharedDataService
   ) { }
 
-  public menuItems = [
-    {
-      name: 'Dashboard',
-      icon: 'fab fa-dashcube fa-fw',
-      hasSubmenu: false,
-      url: 'dashboard',
-      allowedRoles: ['admin', 'editor']
-    },
-    {
-      name: 'Clientes',
-      icon: 'fas fa-users fa-fw',
-      url: null,
-      hasSubmenu: true,
-      isOpen: false,
-      submenu: [
-        { 
-          name: 'Agregar Cliente',
-          icon: 'fas fa-plus fa-fw',
-          url: '/admin/user/customer/new',
-          allowedRoles: ['admin']
-        },
-        { 
-          name: 'Lista de clientes',
-          icon: 'fas fa-clipboard-list fa-fw',
-          url: 'customers',
-          allowedRoles: ['admin', 'viewer']
-        },
-        {
-          name: 'Buscar cliente',
-          icon: 'fas fa-search fa-fw',
-          url: null,
-          allowedRoles: ['admin', 'editor']
-        }
-      ]
-    },
-    {
-      name: 'Items',
-      icon: 'fas fa-pallet fa-fw',
-      url: null,
-      hasSubmenu: true,
-      isOpen: false,
-      allowedRoles: ['sysadmin'],
-      submenu: [
-        { 
-          name: 'Agregar item',
-          icon: 'fas fa-plus fa-fw',
-          url: null,
-          allowedRoles: ['sysadmin']
-        },
-        { 
-          name: 'Lista de items',
-          icon: 'fas fa-clipboard-list fa-fw',
-          url: 'items',
-          allowedRoles: ['sysadmin']
-        },
-        {
-          name: 'Buscar item',
-          icon: 'fas fa-search fa-fw',
-          url: null,
-          allowedRoles: ['sysadmin']
-        }
-      ]
-    },
-    {
-      name: 'Modelo Items',
-      icon: 'fas fa-pallet fa-fw',
-      url: null,
-      hasSubmenu: true,
-      isOpen: false,
-      allowedRoles: ['admin'],
-      submenu: [
-        { 
-          name: 'Agregar modelo item',
-          icon: 'fas fa-plus fa-fw',
-          url: null,
-          allowedRoles: ['admin']
-        },
-        { 
-          name: 'Lista de modelo items',
-          icon: 'fas fa-clipboard-list fa-fw',
-          url: 'models-items',
-          allowedRoles: ['admin']
-        },
-        {
-          name: 'Buscar modelo item',
-          icon: 'fas fa-search fa-fw',
-          url: null,
-          allowedRoles: ['admin']
-        }
-      ]
-    },
-    {
-      name: 'Préstamos',
-      icon: 'fas fa-file-invoice-dollar fa-fw',
-      url: null,
-      hasSubmenu: true,
-      isOpen: false,
-      submenu: [
-        { 
-          name: 'Nuevo préstamo',
-          icon: 'fas fa-plus fa-fw',
-          url: null
-        },
-        { 
-          name: 'Lista de préstamos',
-          icon: 'fas fa-clipboard-list fa-fw',
-          url: null
-        },
-        {
-          name: 'Buscar préstamos',
-          icon: 'fas fa-search fa-fw',
-          url: null
-        }
-      ]
-    },
-    {
-      name: 'Usuarios',
-      icon: 'fas fa-user-secret fa-fw',
-      url: null,
-      hasSubmenu: true,
-      isOpen: false,
-      submenu: [
-        { 
-          name: 'Nuevo usuario',
-          icon: 'fas fa-plus fa-fw',
-          url: '/admin/application/user/new',
-          allowedRoles: ['admin', 'editor']
-        },
-        { 
-          name: 'Lista de usuarios',
-          icon: 'fas fa-clipboard-list fa-fw',
-          url: '/admin/application/users',
-          allowedRoles: ['sysadmin']
-        },
-        {
-          name: 'Buscar usuarios',
-          icon: 'fas fa-search fa-fw',
-          url: null,
-          allowedRoles: ['sysadmin']
-        }
-      ]
-    },
-    {
-      name: 'Empresa',
-      icon: 'fas fa-store-alt fa-fw',
-      url: null,
-      hasSubmenu: false,
-      allowedRoles: ['admin']
-    }
-  ];
-
+  public menuItems: any;
   public selectedCompany: any;
   public filteredNavigation: any[] = [];
 
   ngOnInit(): void {
+
+    let cmp_uuid = JSON.parse(localStorage.getItem('company')!).cmp_uuid;
+
+    this.menuItems = [
+      {
+        name: 'Dashboard',
+        icon: 'fab fa-dashcube fa-fw',
+        hasSubmenu: false,
+        url: 'dashboard',
+        allowedRoles: ['admin', 'editor']
+      },
+      {
+        name: 'Clientes',
+        icon: 'fas fa-users fa-fw',
+        url: null,
+        hasSubmenu: true,
+        isOpen: false,
+        submenu: [
+          { 
+            name: 'Agregar Cliente',
+            icon: 'fas fa-plus fa-fw',
+            url: '/admin/user/customer/new',
+            allowedRoles: ['admin']
+          },
+          { 
+            name: 'Lista de clientes',
+            icon: 'fas fa-clipboard-list fa-fw',
+            url: 'customers',
+            allowedRoles: ['admin', 'viewer']
+          },
+          {
+            name: 'Buscar cliente',
+            icon: 'fas fa-search fa-fw',
+            url: null,
+            allowedRoles: ['admin', 'editor']
+          }
+        ]
+      },
+      {
+        name: 'Items',
+        icon: 'fas fa-pallet fa-fw',
+        url: null,
+        hasSubmenu: true,
+        isOpen: false,
+        allowedRoles: ['sysadmin'],
+        submenu: [
+          { 
+            name: 'Agregar item',
+            icon: 'fas fa-plus fa-fw',
+            url: null,
+            allowedRoles: ['sysadmin']
+          },
+          { 
+            name: 'Lista de items',
+            icon: 'fas fa-clipboard-list fa-fw',
+            url: 'items',
+            allowedRoles: ['sysadmin']
+          },
+          {
+            name: 'Buscar item',
+            icon: 'fas fa-search fa-fw',
+            url: null,
+            allowedRoles: ['sysadmin']
+          }
+        ]
+      },
+      {
+        name: 'Modelo Items',
+        icon: 'fas fa-pallet fa-fw',
+        url: null,
+        hasSubmenu: true,
+        isOpen: false,
+        allowedRoles: ['admin'],
+        submenu: [
+          { 
+            name: 'Agregar modelo item',
+            icon: 'fas fa-plus fa-fw',
+            url: null,
+            allowedRoles: ['admin']
+          },
+          { 
+            name: 'Lista de modelo items',
+            icon: 'fas fa-clipboard-list fa-fw',
+            url: 'models-items',
+            allowedRoles: ['admin']
+          },
+          {
+            name: 'Buscar modelo item',
+            icon: 'fas fa-search fa-fw',
+            url: null,
+            allowedRoles: ['admin']
+          }
+        ]
+      },
+      {
+        name: 'Trabajos',
+        icon: 'fas fa-file-invoice-dollar fa-fw',
+        url: null,
+        hasSubmenu: true,
+        isOpen: false,
+        submenu: [
+          { 
+            name: 'Nuevo trabajo',
+            icon: 'fas fa-plus fa-fw',
+            url: null
+          },
+          { 
+            name: 'Lista de trabajos',
+            icon: 'fas fa-clipboard-list fa-fw',
+            url: 'works'
+          },
+          {
+            name: 'Buscar trabajos',
+            icon: 'fas fa-search fa-fw',
+            url: null
+          }
+        ]
+      },
+      {
+        name: 'Usuarios',
+        icon: 'fas fa-user-secret fa-fw',
+        url: null,
+        hasSubmenu: true,
+        isOpen: false,
+        submenu: [
+          { 
+            name: 'Nuevo usuario',
+            icon: 'fas fa-plus fa-fw',
+            url: '/admin/application/user/new',
+            allowedRoles: ['admin', 'editor']
+          },
+          { 
+            name: 'Lista de usuarios',
+            icon: 'fas fa-clipboard-list fa-fw',
+            url: '/admin/application/users',
+            allowedRoles: ['sysadmin']
+          },
+          {
+            name: 'Buscar usuarios',
+            icon: 'fas fa-search fa-fw',
+            url: null,
+            allowedRoles: ['sysadmin']
+          }
+        ]
+      },
+      {
+        name: 'Empresa',
+        icon: 'fas fa-store-alt fa-fw',
+        url: '/admin/user/company-profile/' + cmp_uuid,
+        hasSubmenu: false,
+        allowedRoles: ['admin']
+      }
+    ];
+
     this._sharedDataService.selectedCompany$.subscribe((company) => {
       if(company) {
         this.selectedCompany = company.cmp;
