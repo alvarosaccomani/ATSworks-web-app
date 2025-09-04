9c9b2e0371512d282e127d0ea6d0297a8ddbbd8a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-04 13:14:21 -0300

    i add the company-profile/:cmp_uuid route in user.routes.ts

--- src/app/admin/user/user.routes.ts
+++ src/app/admin/user/user.routes.ts
@@ -6,17 +6,19 @@ import { CustomersComponent } from './customers/customers.component';
 import { CustomerComponent } from './customer/customer.component';
 import { ModelsItemsComponent } from './models-items/models-items.component';
 import { ModelItemComponent } from './model-item/model-item.component';
+import { CompanyProfileComponent } from './company-profile/company-profile.component';
 
 export const USER_ROUTES: Routes = [
     {
         path: '',
         component: UserLayoutComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'customers', component: CustomersComponent},
-            { path: 'customer/:cus_uuid', component: CustomerComponent},
-            { path: 'models-items', component: ModelsItemsComponent},
-            { path: 'model-item/:itm_uuid/:cmpitm_uuid/:mitm_uuid', component: ModelItemComponent}
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'customers', component: CustomersComponent },
+            { path: 'customer/:cus_uuid', component: CustomerComponent },
+            { path: 'models-items', component: ModelsItemsComponent },
+            { path: 'model-item/:itm_uuid/:cmpitm_uuid/:mitm_uuid', component: ModelItemComponent },
+            { path: 'company-profile/:cmp_uuid', component: CompanyProfileComponent }
         ]
     }
 ];
