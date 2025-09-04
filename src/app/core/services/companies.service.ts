3ec6f117d22a053211141d7fb5cf77d19b661aad
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-04 13:09:09 -0300

    add method getCompanyById to companies service

--- src/app/core/services/companies.service.ts
+++ src/app/core/services/companies.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
+import { CompanyResults } from '../interfaces/company';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+
+  public getCompanyById(cmp_uuid: string): Observable<CompanyResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.get<CompanyResults>(this._GlobalService.url + 'company/' + cmp_uuid, {headers:headers})
+  }
 }
