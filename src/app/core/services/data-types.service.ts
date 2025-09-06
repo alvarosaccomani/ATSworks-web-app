20f5d43398b4d6d3aa1fdb6b1578041c154bdfe2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-06 17:53:38 -0300

    i add the getDataTypes method to the data-type service

--- src/app/core/services/data-types.service.ts
+++ src/app/core/services/data-types.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataTypesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+
+  public getDataTypes(filter: string, page?: number, perPage?: number): Observable<any> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
+    }
+
+    return this._http.get(this._GlobalService.url + 'data-types/' + filter, {headers:headers})
+  }
 }
