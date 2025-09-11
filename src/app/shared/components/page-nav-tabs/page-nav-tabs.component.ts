aa7fa19353c3889f80fcc1e180456fd91d42445a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-11 10:08:29 -0300

    add input data in page-nav-tabs component

--- src/app/shared/components/page-nav-tabs/page-nav-tabs.component.ts
+++ src/app/shared/components/page-nav-tabs/page-nav-tabs.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-page-nav-tabs',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-nav-tabs.component.html',
   styleUrl: './page-nav-tabs.component.scss'
 })
 export class PageNavTabsComponent {
+
+  @Input() data: any[] = []; // Array de datos de entrada
 
 }
