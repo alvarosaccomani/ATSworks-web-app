fd58c15f49573e7df7cf945c69555a2aa8078a6c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-10 23:14:29 -0300

    i add method getCustomers from _customersService and inject _sharedDataService in customers component

--- src/app/admin/user/customers/customers.component.ts
+++ src/app/admin/user/customers/customers.component.ts
@@ -4,6 +4,7 @@ import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CustomersService } from '../../../core/services/customers.service';
 import { CustomerResults } from '../../../core/interfaces/customer';
+import { SharedDataService } from '../../../core/services/shared-data.service';
 
 @Component({
   selector: 'app-customers',
@@ -19,10 +20,19 @@ export class CustomersComponent implements OnInit {
   public customers$!: Observable<CustomerResults>;
 
   constructor(
-    private _customersService: CustomersService
+    private _customersService: CustomersService,
+    private _sharedDataService: SharedDataService
   ) { }
   
   ngOnInit(): void {
-    this.customers$ = this._customersService.getCustomers('');
+    let cmp_uuid = JSON.parse(localStorage.getItem('company')!).cmp_uuid;
+
+    this.customers$ = this._customersService.getCustomers(cmp_uuid);
+    this._sharedDataService.selectedCompany$.subscribe((company) => {
+      if (company) {
+        console.info(company);
+        this.customers$ = this._customersService.getCustomers(company.cmp.cmp_uuid);
+      }
+    });
   }
 }
