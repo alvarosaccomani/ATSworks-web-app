23a0a2b8615e5a8172b1502bde35b2b29e5acf50
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-11 18:15:21 -0300

    i add page-nav-tabs component into items component

--- src/app/admin/application/items/items.component.ts
+++ src/app/admin/application/items/items.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
+import { PageNavTabsComponent } from '../../../shared/components/page-nav-tabs/page-nav-tabs.component';
 import { ItemResults } from '../../../core/interfaces/item';
 import { ItemsService } from '../../../core/services/items.service';
 
@@ -9,7 +10,8 @@ import { ItemsService } from '../../../core/services/items.service';
   selector: 'app-items',
   imports: [
     AsyncPipe,
-    RouterLink
+    RouterLink,
+    PageNavTabsComponent
   ],
   templateUrl: './items.component.html',
   styleUrl: './items.component.scss'
@@ -17,6 +19,19 @@ import { ItemsService } from '../../../core/services/items.service';
 export class ItemsComponent implements OnInit {
 
   public items$!: Observable<ItemResults>;
+
+  public dataTabs: any = [
+    {
+      url: ['/admin/application/item', 'new'],
+      icon: "fas fa-plus fa-fw",
+      title: "NUEVO ITEM"
+    },
+    {
+       url: ['/admin/application/items'],
+       icon: "fas fa-clipboard-list fa-fw",
+       title: "LISTA DE ITEMS"
+    }
+  ] 
   
     constructor(
       private _itemsService: ItemsService
