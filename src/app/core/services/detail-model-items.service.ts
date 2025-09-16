aafe5d29c9bebc3b12d778d6c2fb6ee7b4b636a2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-16 10:30:51 -0300

    add method insertDetailModelItem to detail-model-items service

--- src/app/core/services/detail-model-items.service.ts
+++ src/app/core/services/detail-model-items.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailModelItemsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+
+  public insertDetailModelItem(detailModelItem: any): Observable<any> {
+    let params = JSON.stringify(detailModelItem);
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.post(this._GlobalService.url + 'detail-model-item', params, {headers:headers});
+  }
 }
