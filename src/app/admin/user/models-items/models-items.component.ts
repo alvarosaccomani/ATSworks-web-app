e036934cd8200cafde66a381f4c3b56f5f969fb4
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-17 22:08:51 -0300

    i add method deleteModelItem in model-items component

--- src/app/admin/user/models-items/models-items.component.ts
+++ src/app/admin/user/models-items/models-items.component.ts
@@ -5,8 +5,10 @@ import { Observable } from 'rxjs';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { PageNavTabsComponent } from '../../../shared/components/page-nav-tabs/page-nav-tabs.component';
 import { ModelItemsService } from '../../../core/services/model-items.service';
-import { ModelItemResults } from '../../../core/interfaces/model-item';
+import { ModelItemInterface, ModelItemResults } from '../../../core/interfaces/model-item';
 import { SharedDataService } from '../../../core/services/shared-data.service';
+
+declare var Swal: any;
 
 @Component({
   selector: 'app-models-items',
@@ -55,5 +57,31 @@ export class ModelsItemsComponent {
         this.modelItems$ = this._modelItemsService.getModelItems(company.cmp.cmp_uuid);
       }
     });
+  }
+
+  public deleteModelItem(modelItem: ModelItemInterface) {
+    Swal.fire({
+        title: '¿Desea eliminar el Modelo de Item?',
+        text: "You are about to close the session and exit the system",
+        type: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminar!',
+        cancelButtonText: 'No, cancelar'
+      }).then((result: any) => {
+        if (result.value) {
+          this._modelItemsService.deleteModelItem(modelItem.cmp_uuid!, modelItem.itm_uuid!, modelItem.cmpitm_uuid!, modelItem.mitm_uuid!)
+            .subscribe(
+              response => {
+                console.info(response);
+                this.modelItems$ = this._modelItemsService.getModelItems(modelItem.cmp_uuid!);
+              },
+              error => {
+                console.log(<any>error);
+              }
+            );
+        }
+      });
   }
 }
