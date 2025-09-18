74a94cea9b09971bb7e01f35d9892f48f8a5a665
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-18 17:33:06 -0300

    i add the usr property to the user-rol-company interface

--- src/app/core/interfaces/user-rol-company/user-rol-company.interface.ts
+++ src/app/core/interfaces/user-rol-company/user-rol-company.interface.ts
@@ -1,11 +1,13 @@
 import { CompanyInterface } from "../company"
 import { RolInterface } from "../rol"
+import { UserInterface } from "../user/user.interface"
 
 export interface UserRolCompanyInterface {
     cmp_uuid: string | null,
     cmp: CompanyInterface | null,
     usrrolcmp_uuid: string | null,
     usr_uuid: string | null,
+    usr: UserInterface | null,
     rol_uuid: string | null,
     rol: RolInterface | null,
     usrrolcmp_createdat: Date | null,
