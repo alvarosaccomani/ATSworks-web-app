dc9fab01a9f38ec47f430c3a79f60d724150bd72
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-18 12:46:28 -0300

    add methods insertWorkDetail and updateWorkDetail to works-details service

--- src/app/core/services/works-details.service.ts
+++ src/app/core/services/works-details.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorksDetailsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+
+  public insertWorkDetail(workDetail: any): Observable<any> {
+    let params = JSON.stringify(workDetail);
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.post(this._GlobalService.url + 'work-detail', params, {headers:headers});
+  }
+
+  public updateWorkDetail(workDetail: any): Observable<any> {
+    let params = JSON.stringify(workDetail);
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.put(this._GlobalService.url + 'work-detail/' + workDetail.wrk_uuid + '/' + workDetail.dwrk_uuid, params, {headers:headers});
+  }
 }
