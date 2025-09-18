313a3da18171e4351a34887126eeacccff41cb85
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-18 18:20:14 -0300

    i add the adr, wrks, wrk_user, wrk_operator and mitm properties to the work interface

--- src/app/core/interfaces/work/work.interface.ts
+++ src/app/core/interfaces/work/work.interface.ts
@@ -1,19 +1,28 @@
+import { AddressInterface } from "../address";
+import { WorkStateInterface } from "../work-state";
+import { UserInterface } from "../user";
+import { ModelItemInterface } from "../model-item";
 import { WorkDetailInterface } from "../work-detail";
 
 export interface WorkInterface {
     cmp_uuid: string | null,
     wrk_uuid: string | null,
     adr_uuid: string | null,
+    adr: AddressInterface | null,
     wrk_description: string | null,
     wrk_workdate: Date | null,
     wrk_workdateinit: Date | null,
     wrk_workdatefinish: Date | null,
     wrks_uuid: string | null,
+    wrks: WorkStateInterface | null,
     wrk_user_uuid: string | null,
+    wrk_user: UserInterface | null,
     wrk_operator_uuid: string | null,
+    wrk_operator: UserInterface | null,
     itm_uuid: string | null,
     cmpitm_uuid: string | null,
     mitm_uuid: string | null,
+    mitm: ModelItemInterface | null,
 	wrk_createdat: Date | null,
     wrk_updatedat: Date | null,
     workDetails?: WorkDetailInterface[]
