e9ade42749992c244e80e022454af933c90bbd86
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-18 22:44:53 -0300

    i add the cus property to the address interface

--- src/app/core/interfaces/address/address.interface.ts
+++ src/app/core/interfaces/address/address.interface.ts
@@ -1,7 +1,10 @@
+import { CustomerInterface } from "../customer"
+
 export interface AddressInterface {
     cmp_uuid: string | null,
     adr_uuid: string | null,
     cus_uuid: string | null,
+    cus: CustomerInterface | null,
     adr_address: string | null,
     adr_city: string | null,
     adr_province: string | null,
