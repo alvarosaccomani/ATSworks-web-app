b8dce1fc6752c3a38adad497514ec0a8894dd0f8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-19 15:10:09 -0300

    i add the address/:cus_uuid/:adr_uuid route in user.routes.ts

--- src/app/admin/user/user.routes.ts
+++ src/app/admin/user/user.routes.ts
@@ -4,6 +4,7 @@ import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomerComponent } from './customer/customer.component';
+import { AddressComponent } from './address/address.component';
 import { ModelsItemsComponent } from './models-items/models-items.component';
 import { ModelItemComponent } from './model-item/model-item.component';
 import { WorksComponent } from './works/works.component';
@@ -18,6 +19,7 @@ export const USER_ROUTES: Routes = [
             { path: 'dashboard', component: DashboardComponent },
             { path: 'customers', component: CustomersComponent },
             { path: 'customer/:cus_uuid', component: CustomerComponent },
+            { path: 'address/:cus_uuid/:adr_uuid', component: AddressComponent },
             { path: 'models-items', component: ModelsItemsComponent },
             { path: 'model-item/:itm_uuid/:cmpitm_uuid/:mitm_uuid', component: ModelItemComponent },
             { path: 'works', component: WorksComponent },
