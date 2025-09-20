5e43cce132103f2abe1616ecb4ea0e0b99077d25
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-20 09:05:33 -0300

    i add multi-step component to project

---
+++ src/app/shared/components/multi-step/multi-step.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-step',
+  imports: [],
+  templateUrl: './multi-step.component.html',
+  styleUrl: './multi-step.component.scss'
+})
+export class MultiStepComponent {
+
+}
