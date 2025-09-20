298e69e5da757b3b8724d68d3be34d0b1ad8004c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-20 19:30:23 -0300

    add insertWorAttachment to works-attachments service

--- src/app/core/services/works-attachments.service.ts
+++ src/app/core/services/works-attachments.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorksAttachmentsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+
+  public insertWorAttachment(workAttachment: any): Observable<any> {
+    let params = JSON.stringify(workAttachment);
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.post(this._GlobalService.url + 'work-attachment', params, {headers:headers});
+  }
 }
