2398c65ba07d44e3f8909573408ebebdb1d46760
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-25 21:11:07 -0300

    add method getDashboards to dashboards service

--- src/app/core/services/dashboards.service.ts
+++ src/app/core/services/dashboards.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
+import { DashboardResults  } from '../interfaces/dashboard';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+
+  public getDashboards(cmp_uuid: string, filter?: string, page?: number, perPage?: number): Observable<DashboardResults> {
+      let headers = new HttpHeaders().set('content-type','application/json');
+  
+      if(page && perPage) {
+        filter = `${filter}/${page}/${perPage}`;
+      }
+  
+      return this._http.get<DashboardResults>(this._GlobalService.url + 'dashboards/' + cmp_uuid + '/' + filter, {headers:headers})
+    }
 }
