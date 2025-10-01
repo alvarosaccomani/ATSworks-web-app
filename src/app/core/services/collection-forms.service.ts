32eeb8d12b4826029be98b446d6fb57f50ad537d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-01 16:29:56 -0300

    add method getCollectionForms to collection-form service

--- src/app/core/services/collection-forms.service.ts
+++ src/app/core/services/collection-forms.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalService } from './global.service';
+import { CollectionFormResults } from '../interfaces/collection-form';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CollectionFormsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _GlobalService: GlobalService
+  ) { }
+  
+  public getCollectionForms(cmp_uuid: string, filter?: string, page?: number, perPage?: number): Observable<CollectionFormResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
+    }
+
+    return this._http.get<CollectionFormResults>(this._GlobalService.url + 'collection-forms/' + cmp_uuid + '/' + filter, {headers:headers})
+  }
 }
