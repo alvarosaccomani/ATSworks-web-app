d9b43e630abc9d8bfc2f9718eba06ddeec70ea44
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-01 16:28:13 -0300

    i add the cfrm_uuid property to the customer interface

--- src/app/core/interfaces/customer/customer.interface.ts
+++ src/app/core/interfaces/customer/customer.interface.ts
@@ -6,6 +6,7 @@ export interface CustomerInterface {
     cus_fullname: string | null,
     cus_email: string | null,
     cus_phone: string | null,    
+    cfrm_uuid: string | null,
     usr_uuid: string | null,
     cus_createdat: Date | null,
     cus_updatedat: Date | null,
