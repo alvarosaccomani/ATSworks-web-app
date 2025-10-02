96e5cb439d449ea4f71c68d5888319b1a63e7dc2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-02 13:25:58 -0300

    i add the no-company route in user.routes.ts

--- src/app/admin/user/user.routes.ts
+++ src/app/admin/user/user.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { UserLayoutComponent } from './user-layout/user-layout.component';
+import { NoCompanyComponent } from './no-company/no-company.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomerComponent } from './customer/customer.component';
@@ -17,6 +18,7 @@ export const USER_ROUTES: Routes = [
         path: '',
         component: UserLayoutComponent,
         children: [
+            { path: 'no-company', component: NoCompanyComponent},
             { path: 'dashboard', component: DashboardComponent },
             { path: 'customers', component: CustomersComponent },
             { path: 'customer/:cus_uuid', component: CustomerComponent },
