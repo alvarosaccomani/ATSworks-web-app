a8e69006f8a1958f3b08e26789f7a8512751aa51
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-02 18:54:43 -0300

    i add output property imageSelected to image component

--- src/app/shared/components/image/image.component.ts
+++ src/app/shared/components/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-image',
@@ -8,16 +8,24 @@ import { Component } from '@angular/core';
 })
 export class ImageComponent {
 
-  public imageSrc!: string;
+  @Output() imageSelected = new EventEmitter<any>(); // Evento para emitir la imagen
+
+  public imageSrc!: any;
   public filesToUpload: Array<File> = [];
-  
+
   public changeImage(e: any): void {
     if(e.target.files && e.target.files.length) {
       this.filesToUpload = <Array<File>>e.target.files;
       var reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
       reader.onload = (event: any) => {
-        this.imageSrc = event.target.result;
+        this.imageSrc = {
+          name: this.filesToUpload[0].name,
+          size: this.filesToUpload[0].size,
+          type: this.filesToUpload[0].type,
+          base64: event.target.result
+        }
+        this.imageSelected.emit(this.imageSrc); // Emitir la imagen capturada
       };
     }
   }
