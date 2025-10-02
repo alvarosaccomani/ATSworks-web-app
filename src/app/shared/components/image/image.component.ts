f3ff806a8054a73dd15aa0934401ecae60e57099
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-02 18:11:35 -0300

    add method changeImage in image component

--- src/app/shared/components/image/image.component.ts
+++ src/app/shared/components/image/image.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class ImageComponent {
 
+  public imageSrc!: string;
+  public filesToUpload: Array<File> = [];
+  
+  public changeImage(e: any): void {
+    if(e.target.files && e.target.files.length) {
+      this.filesToUpload = <Array<File>>e.target.files;
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = (event: any) => {
+        this.imageSrc = event.target.result;
+      };
+    }
+  }
+
 }
