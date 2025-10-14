3bbb837886f44e1fe610db6e1384be2602dfe1c8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-14 12:34:25 -0300

    i replace this._GlobalService.url by environment.apiUrl in detail-model-items.service.ts

--- src/app/core/services/detail-model-items.service.ts
+++ src/app/core/services/detail-model-items.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GlobalService } from './global.service';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +9,20 @@ import { GlobalService } from './global.service';
 export class DetailModelItemsService {
 
   constructor(
-    private _http: HttpClient,
-    private _GlobalService: GlobalService
+    private _http: HttpClient
   ) { }
 
   public insertDetailModelItem(detailModelItem: any): Observable<any> {
     let params = JSON.stringify(detailModelItem);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.post(this._GlobalService.url + 'detail-model-item', params, {headers:headers});
+    return this._http.post(environment.apiUrl + 'detail-model-item', params, {headers:headers});
   }
 
   public updateDetailModelItem(detailModelItem: any): Observable<any> {
     let params = JSON.stringify(detailModelItem);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.put(this._GlobalService.url + 'detail-model-item/' + detailModelItem.cmp_uuid + '/' + detailModelItem.itm_uuid  + '/' + detailModelItem.cmpitm_uuid + '/' + detailModelItem.mitm_uuid + '/' + detailModelItem.dmitm_uuid, params, {headers:headers});
+    return this._http.put(environment.apiUrl + 'detail-model-item/' + detailModelItem.cmp_uuid + '/' + detailModelItem.itm_uuid  + '/' + detailModelItem.cmpitm_uuid + '/' + detailModelItem.mitm_uuid + '/' + detailModelItem.dmitm_uuid, params, {headers:headers});
   }
 }
