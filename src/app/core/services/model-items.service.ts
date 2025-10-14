e7b2221d9c8f4c2b6fbddc0e16a9f3a146914476
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-14 12:34:58 -0300

    i replace this._GlobalService.url by environment.apiUrl in model-items.service.ts

--- src/app/core/services/model-items.service.ts
+++ src/app/core/services/model-items.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GlobalService } from './global.service';
+import { environment } from '../../../environments/environment';
 import { ModelItemResults } from '../interfaces/model-item';
 
 @Injectable({
@@ -10,8 +10,7 @@ import { ModelItemResults } from '../interfaces/model-item';
 export class ModelItemsService {
 
   constructor(
-    private _http: HttpClient,
-    private _GlobalService: GlobalService
+    private _http: HttpClient
   ) { }
 
   public getModelItems(cmp_uuid: string, filter?: string, page?: number, perPage?: number): Observable<ModelItemResults> {
@@ -21,32 +20,32 @@ export class ModelItemsService {
       filter = `${filter}/${page}/${perPage}`;
     }
 
-    return this._http.get<ModelItemResults>(this._GlobalService.url + 'model-items/' + cmp_uuid + '/' + filter, {headers:headers})
+    return this._http.get<ModelItemResults>(environment.apiUrl + 'model-items/' + cmp_uuid + '/' + filter, {headers:headers})
   }
 
   public getModelItemById(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Observable<any> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.get(this._GlobalService.url + 'model-item/' + cmp_uuid + '/' + itm_uuid + '/' + cmpitm_uuid + '/' + mitm_uuid, {headers:headers});
+    return this._http.get(environment.apiUrl + 'model-item/' + cmp_uuid + '/' + itm_uuid + '/' + cmpitm_uuid + '/' + mitm_uuid, {headers:headers});
   }
 
   public saveModelItem(modelItem: any): Observable<any> {
     let params = JSON.stringify(modelItem);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.post(this._GlobalService.url + 'model-item', params, {headers:headers});
+    return this._http.post(environment.apiUrl + 'model-item', params, {headers:headers});
   }
 
   public updateModelItem(modelItem: any): Observable<any> {
     let params = JSON.stringify(modelItem);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.put(this._GlobalService.url + 'model-item/' + modelItem.cmp_uuid + '/' + modelItem.itm_uuid + '/' + modelItem.cmpitm_uuid + '/' + modelItem.mitm_uuid, params, {headers:headers});
+    return this._http.put(environment.apiUrl + 'model-item/' + modelItem.cmp_uuid + '/' + modelItem.itm_uuid + '/' + modelItem.cmpitm_uuid + '/' + modelItem.mitm_uuid, params, {headers:headers});
   }
 
   public deleteModelItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Observable<any> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.delete(this._GlobalService.url + 'model-item/' + cmp_uuid + '/' + itm_uuid + '/' + cmpitm_uuid + '/' + mitm_uuid, {headers:headers});
+    return this._http.delete(environment.apiUrl + 'model-item/' + cmp_uuid + '/' + itm_uuid + '/' + cmpitm_uuid + '/' + mitm_uuid, {headers:headers});
   }
 }
