91a10c0fd7be453260879bdd84a55d3742bd70bf
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-14 12:33:29 -0300

    i replace this._GlobalService.url by environment.apiUrl in customers.service.ts

--- src/app/core/services/customers.service.ts
+++ src/app/core/services/customers.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GlobalService } from './global.service';
+import { environment } from '../../../environments/environment';
 import { CustomerResults  } from '../interfaces/customer';
 
 @Injectable({
@@ -10,8 +10,7 @@ import { CustomerResults  } from '../interfaces/customer';
 export class CustomersService {
 
   constructor(
-    private _http: HttpClient,
-    private _GlobalService: GlobalService
+    private _http: HttpClient
   ) { }
 
   public getCustomers(cmp_uuid: string, filter?: string, page?: number, perPage?: number): Observable<CustomerResults> {
@@ -21,32 +20,32 @@ export class CustomersService {
       filter = `${filter}/${page}/${perPage}`;
     }
 
-    return this._http.get<CustomerResults>(this._GlobalService.url + 'customers/' + cmp_uuid + '/' + filter, {headers:headers})
+    return this._http.get<CustomerResults>(environment.apiUrl + 'customers/' + cmp_uuid + '/' + filter, {headers:headers})
   }
 
   public getCustomerById(cmp_uuid: string, cus_uuid?: string): Observable<any> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.get(this._GlobalService.url + 'customer/' + cmp_uuid + '/' + cus_uuid, {headers:headers});
+    return this._http.get(environment.apiUrl + 'customer/' + cmp_uuid + '/' + cus_uuid, {headers:headers});
   }
 
   public saveCustomer(customer: any): Observable<any> {
     let params = JSON.stringify(customer);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.post(this._GlobalService.url + 'customer', params, {headers:headers});
+    return this._http.post(environment.apiUrl + 'customer', params, {headers:headers});
   }
 
   public updateCustomer(customer: any): Observable<any> {
     let params = JSON.stringify(customer);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.put(this._GlobalService.url + 'customer/' + customer.cmp_uuid + '/' + customer.cus_uuid, params, {headers:headers});
+    return this._http.put(environment.apiUrl + 'customer/' + customer.cmp_uuid + '/' + customer.cus_uuid, params, {headers:headers});
   }
 
   public deleteCustomer(cmp_uuid: string, cus_uuid: string): Observable<any> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.delete(this._GlobalService.url + 'customer/' + cmp_uuid + '/' + cus_uuid, {headers:headers});
+    return this._http.delete(environment.apiUrl + 'customer/' + cmp_uuid + '/' + cus_uuid, {headers:headers});
   }
 }
