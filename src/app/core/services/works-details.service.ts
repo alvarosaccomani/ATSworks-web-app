c8cb442dfecc86ed79e23f91e6d566086879631d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-14 12:36:20 -0300

    i replace this._GlobalService.url by environment.apiUrl in works-details.service.ts

--- src/app/core/services/works-details.service.ts
+++ src/app/core/services/works-details.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GlobalService } from './global.service';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +9,20 @@ import { GlobalService } from './global.service';
 export class WorksDetailsService {
 
   constructor(
-    private _http: HttpClient,
-    private _GlobalService: GlobalService
+    private _http: HttpClient
   ) { }
 
   public insertWorkDetail(workDetail: any): Observable<any> {
     let params = JSON.stringify(workDetail);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.post(this._GlobalService.url + 'work-detail', params, {headers:headers});
+    return this._http.post(environment.apiUrl + 'work-detail', params, {headers:headers});
   }
 
   public updateWorkDetail(workDetail: any): Observable<any> {
     let params = JSON.stringify(workDetail);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.put(this._GlobalService.url + 'work-detail/' + workDetail.cmp_uuid + '/' + workDetail.wrk_uuid + '/' + workDetail.dwrk_uuid, params, {headers:headers});
+    return this._http.put(environment.apiUrl + 'work-detail/' + workDetail.cmp_uuid + '/' + workDetail.wrk_uuid + '/' + workDetail.dwrk_uuid, params, {headers:headers});
   }
 }
