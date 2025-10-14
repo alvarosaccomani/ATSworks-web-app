c41d1935d670f412d9fdfb178fed724c6a99a5aa
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-14 12:36:36 -0300

    i replace this._GlobalService.url by environment.apiUrl in works.service.ts

--- src/app/core/services/works.service.ts
+++ src/app/core/services/works.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GlobalService } from './global.service';
+import { environment } from '../../../environments/environment';
 import { WorkResults } from '../interfaces/work';
 
 @Injectable({
@@ -10,8 +10,7 @@ import { WorkResults } from '../interfaces/work';
 export class WorksService {
 
   constructor(
-    private _http: HttpClient,
-    private _GlobalService: GlobalService
+    private _http: HttpClient
   ) { }
 
   public getWorks(cmp_uuid: string, filter?: string, page?: number, perPage?: number): Observable<WorkResults> {
@@ -21,32 +20,32 @@ export class WorksService {
       filter = `${filter}/${page}/${perPage}`;
     }
 
-    return this._http.get<WorkResults>(this._GlobalService.url + 'works/' + cmp_uuid + '/' + filter, {headers:headers})
+    return this._http.get<WorkResults>(environment.apiUrl + 'works/' + cmp_uuid + '/' + filter, {headers:headers})
   }
 
   public getWorkById(cmp_uuid: string, wrk_uuid: string): Observable<any> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.get(this._GlobalService.url + 'work/' + cmp_uuid + '/' + wrk_uuid, {headers:headers});
+    return this._http.get(environment.apiUrl + 'work/' + cmp_uuid + '/' + wrk_uuid, {headers:headers});
   }
 
   public saveWork(work: any): Observable<any> {
     let params = JSON.stringify(work);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.post(this._GlobalService.url + 'work', params, {headers:headers});
+    return this._http.post(environment.apiUrl + 'work', params, {headers:headers});
   }
 
   public updateWork(work: any): Observable<any> {
     let params = JSON.stringify(work);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.put(this._GlobalService.url + 'work/' + work.cmp_uuid + '/' + work.wrk_uuid, params, {headers:headers});
+    return this._http.put(environment.apiUrl + 'work/' + work.cmp_uuid + '/' + work.wrk_uuid, params, {headers:headers});
   }
 
   public deleteWork(cmp_uuid: string, wrk_uuid: string): Observable<any> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.delete(this._GlobalService.url + 'work/' + cmp_uuid + '/' + wrk_uuid, {headers:headers});
+    return this._http.delete(environment.apiUrl + 'work/' + cmp_uuid + '/' + wrk_uuid, {headers:headers});
   }
 }
