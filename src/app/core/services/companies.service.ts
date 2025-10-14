3286a67783f99e4061fb6d368c75fe81aae80de2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-14 12:32:41 -0300

    i replace this._GlobalService.url by environment.apiUrl in companies.service.ts

--- src/app/core/services/companies.service.ts
+++ src/app/core/services/companies.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GlobalService } from './global.service';
+import { environment } from '../../../environments/environment';
 import { CompanyResults } from '../interfaces/company';
 
 @Injectable({
@@ -10,27 +10,26 @@ import { CompanyResults } from '../interfaces/company';
 export class CompaniesService {
 
   constructor(
-    private _http: HttpClient,
-    private _GlobalService: GlobalService
+    private _http: HttpClient
   ) { }
 
   public getCompanyById(cmp_uuid: string): Observable<CompanyResults> {
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.get<CompanyResults>(this._GlobalService.url + 'company/' + cmp_uuid, {headers:headers})
+    return this._http.get<CompanyResults>(environment.apiUrl + 'company/' + cmp_uuid, {headers:headers})
   }
 
   public saveCompany(company: any): Observable<any> {
     let params = JSON.stringify(company);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.post(this._GlobalService.url + 'company', params, {headers:headers});
+    return this._http.post(environment.apiUrl + 'company', params, {headers:headers});
   }
 
   public updateCompany(company: any): Observable<any> {
     let params = JSON.stringify(company);
     let headers = new HttpHeaders().set('content-type','application/json');
 
-    return this._http.put(this._GlobalService.url + 'company/' + company.cmp_uuid, params, {headers:headers});
+    return this._http.put(environment.apiUrl + 'company/' + company.cmp_uuid, params, {headers:headers});
   }
 }
