306a4e211dacf78f47c48c5e402698a5bfa08112
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-21 19:37:51 -0300

    add methods updateConnectionStatus and isOnline to connection service

--- src/app/core/services/connection.service.ts
+++ src/app/core/services/connection.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectionService {
 
-  constructor() { }
+  private connectionStatus = new BehaviorSubject<boolean>(navigator.onLine);
+  public connectionStatus$ = this.connectionStatus.asObservable();
+
+  constructor() {
+    window.addEventListener('online', this.updateConnectionStatus);
+    window.addEventListener('offline', this.updateConnectionStatus);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('online', this.updateConnectionStatus);
+    window.removeEventListener('offline', this.updateConnectionStatus);
+  }
+
+  private updateConnectionStatus = () => {
+    this.connectionStatus.next(navigator.onLine);
+  };
+
+  public isOnline(): boolean {
+    return navigator.onLine;
+  }
 }
