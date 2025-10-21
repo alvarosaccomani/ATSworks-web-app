163d003e41e37db24af478791f61c5e11404c4e6
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-21 19:38:24 -0300

    i add NzMessageService and ConnectionService to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NzMessageModule, NzMessageService } from 'ng-zorro-antd/message';
+import { ConnectionService } from './core/services/connection.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NzMessageModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ATSworks-web-app';
+  private connectionSubscription!: Subscription;
+
+  constructor(
+    private _nzMessageService: NzMessageService,
+    private _connectionService: ConnectionService
+  ) {}
+
+  ngOnInit() {
+    this.connectionSubscription = this._connectionService.connectionStatus$.subscribe((isOnline) => {
+      if (isOnline) {
+        this._nzMessageService.success('Conexión restablecida', { nzDuration: 3000 });
+      } else {
+        this._nzMessageService.error('Sin conexión a Internet', { nzDuration: 3000 });
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.connectionSubscription.unsubscribe();
+  }
 }
