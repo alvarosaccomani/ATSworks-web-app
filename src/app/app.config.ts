d226e32298b38a0d2738b270d7e23cad6baa2941
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-21 18:38:01 -0300

    i add ng-zorro-antd to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, provideHttpClient } from "@angular/common/http";
 
 import { routes } from './app.routes';
+import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+registerLocaleData(es);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule), provideNzI18n(es_ES), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()
   ]
 };
