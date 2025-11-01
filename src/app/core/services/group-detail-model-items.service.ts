bce7a160c96db691691497200658a718461bcc75
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-01 12:16:59 -0300

    add method getGroupDetailModelItemById to group-detail-model-items service

--- src/app/core/services/group-detail-model-items.service.ts
+++ src/app/core/services/group-detail-model-items.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupDetailModelItemsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  public getGroupDetailModelItemById(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string, gdmitm_uuid: string): Observable<any> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.get(environment.apiUrl + 'group-detail-model-item/' + cmp_uuid + '/' + itm_uuid + '/' + cmpitm_uuid + '/' + mitm_uuid + '/' + gdmitm_uuid, {headers:headers});
+  }
 }
