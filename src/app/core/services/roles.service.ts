3ad03be24146c5ae157ef5d50332e3a62e2b092f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-06 08:59:06 -0300

    add method getRoles to roles service

--- src/app/core/services/roles.service.ts
+++ src/app/core/services/roles.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { RolResults } from '../interfaces/rol';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+    
+    public getRoles(filter?: string, page?: number, perPage?: number): Observable<RolResults> {
+      let headers = new HttpHeaders().set('content-type','application/json');
+  
+      if(page && perPage) {
+        filter = `${filter}/${page}/${perPage}`;
+      }
+  
+      return this._http.get<RolResults>(environment.apiUrl + 'roles/' + filter, {headers:headers})
+    }
 }
