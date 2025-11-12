d55bbc471736769a5a48d65886089cf931905f62
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-12 19:03:56 -0300

    i add methods getCurrentRoute and getCurrentRouteWithoutParams to session service

--- src/app/core/services/route.service.ts
+++ src/app/core/services/route.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteService {
 
-  constructor() { }
+  private currentRoute: string = '';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.urlAfterRedirects;
+        console.log('Ruta actual:', this.currentRoute);
+      });
+  }
+
+  public getCurrentRoute(): string {
+    return this.currentRoute;
+  }
+
+  public getCurrentRouteWithoutParams(): string {
+    return this.currentRoute.split('?')[0];
+  }
 }
