685e30a3d0dbb8b18d50d0bb69b3acaad752e28d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-27 19:02:08 -0300

    i add rol-permissions route in application.routes.ts

--- src/app/admin/application/application.routes.ts
+++ src/app/admin/application/application.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
+import { RolPermissionsComponent } from './rol-permissions/rol-permissions.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
 import { ItemsComponent } from './items/items.component';
@@ -11,6 +12,7 @@ export const APPLICATION_ROUTES: Routes = [
         path: '',
         component: ApplicationLayoutComponent,
         children: [
+            { path: 'rol-permissions', component: RolPermissionsComponent},
             { path: 'users', component: UsersComponent},
             { path: 'user/:usr_uuid', component: UserComponent},
             { path: 'items', component: ItemsComponent},
