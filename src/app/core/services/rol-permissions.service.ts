2df817b3713466d9dabda9652034ef188934ea92
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-28 16:10:11 -0300

    add method getRolPermissions to rol-permissions service

--- src/app/core/services/rol-permissions.service.ts
+++ src/app/core/services/rol-permissions.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { RolPermissionResults } from '../interfaces/rol-permission/rol-permission-results.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolPermissionsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  public getRolPermissions(filter: string, page?: number, perPage?: number): Observable<RolPermissionResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
+    }
+
+    return this._http.get<RolPermissionResults>(environment.apiUrl + 'rol-permissions/' + filter, {headers:headers})
+  }
 }
