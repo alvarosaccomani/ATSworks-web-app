2d74406a3dff01c87128d5b934a7e215f9250017
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-29 19:03:59 -0300

    add methods getPermissionById, savePermission and updatePermission to permissions service

--- src/app/core/services/permissions.service.ts
+++ src/app/core/services/permissions.service.ts
@@ -10,18 +10,38 @@ import { PermissionResults } from '../interfaces/permission/permission-results.i
 export class PermissionsService {
 
   constructor(
-      private _http: HttpClient
-    ) { }
-  
-    public getPermissions(filter?: string, page?: number, perPage?: number): Observable<PermissionResults> {
-      let headers = new HttpHeaders().set('content-type','application/json');
-  
-      if(page && perPage) {
-        filter = `${filter}/${page}/${perPage}`;
-      }
-  
-      return this._http.get<PermissionResults>(environment.apiUrl + 'permissions/' + filter, {headers:headers})
+    private _http: HttpClient
+  ) { }
+
+  public getPermissions(filter?: string, page?: number, perPage?: number): Observable<PermissionResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
     }
+
+    return this._http.get<PermissionResults>(environment.apiUrl + 'permissions/' + filter, {headers:headers})
+  }
+
+  public getPermissionById(per_uuid: string): Observable<any> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.get(environment.apiUrl + 'permission/' + per_uuid, {headers:headers});
+  }
+
+  public savePermission(permission: any): Observable<any> {
+    let params = JSON.stringify(permission);
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.post(environment.apiUrl + 'permission', params, {headers:headers});
+  }
+
+  public updatePermission(permission: any): Observable<any> {
+    let params = JSON.stringify(permission);
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.put(environment.apiUrl + 'permission/' + permission.per_uuid, params, {headers:headers});
+  }
 
   public deletePermission(per_uuid: string): Observable<any> {
     let headers = new HttpHeaders().set('content-type','application/json');
