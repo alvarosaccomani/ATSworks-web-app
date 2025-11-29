fdad6267ddf12fae139bdc8e8e1ea1dbd2834c37
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-29 17:53:22 -0300

    add method getResourcesTypes to resources-types service

--- src/app/core/services/resources-types.service.ts
+++ src/app/core/services/resources-types.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { ResourceTypeResults } from '../interfaces/resource-type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResourcesTypesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  public getResourcesTypes(filter?: string, page?: number, perPage?: number): Observable<ResourceTypeResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
+    }
+
+    return this._http.get<ResourceTypeResults>(environment.apiUrl + 'resources-types/' + filter, {headers:headers})
+  }
 }
