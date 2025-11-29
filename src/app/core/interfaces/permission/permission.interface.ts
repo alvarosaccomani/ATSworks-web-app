560f6c1a117014ca824108bf19b716224abb1334
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-29 19:12:13 -0300

    i add | null to properties in permission interface

--- src/app/core/interfaces/permission/permission.interface.ts
+++ src/app/core/interfaces/permission/permission.interface.ts
@@ -4,11 +4,11 @@ import { ResourceModuleInterface } from "../resource-module";
 export interface PermissionInterface {
     per_uuid: string | null,
     per_slug: string | null,
-    per_description: string,
-    rety_uuid: string,
-    rety?: ResourceTypeInterface,
-    remo_uuid: string,   
-    remo?: ResourceModuleInterface,
+    per_description: string | null,
+    rety_uuid: string | null,
+    rety?: ResourceTypeInterface | null,
+    remo_uuid: string | null,   
+    remo?: ResourceModuleInterface | null,
     per_createdat: Date | null,
     per_updatedat: Date | null
 }
