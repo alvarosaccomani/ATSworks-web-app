bb1413bc304691352ec8fba7916f04f3d8dfad1e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-11-29 17:55:23 -0300

    add method getResourcesModules to resources-modules service

--- src/app/core/services/resources-modules.service.ts
+++ src/app/core/services/resources-modules.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { ResourceModuleResults } from '../interfaces/resource-module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResourcesModulesService {
 
-  constructor() { }
+  constructor(
+      private _http: HttpClient
+  ) { }
+
+  public getResourcesModules(filter?: string, page?: number, perPage?: number): Observable<ResourceModuleResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
+    }
+
+    return this._http.get<ResourceModuleResults>(environment.apiUrl + 'resources-modules/' + filter, {headers:headers})
+  }
 }
