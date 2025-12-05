79427b58eaaaad532bc37ec4efc9d77c3c97c08b
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-05 19:50:37 -0300

    i add | null to properties in route interface

--- src/app/core/interfaces/route/route.interface.ts
+++ src/app/core/interfaces/route/route.interface.ts
@@ -1,12 +1,12 @@
 export interface RouteInterface {
-    cmp_uuid: string,
+    cmp_uuid: string | null,
     rou_uuid: string | null,
     rou_name: string | null,
-    rou_order: number,
-    rou_description: string,
-    rou_bkcolor: string,
-    rou_frcolor: string,
-    rou_active: boolean,
+    rou_order: number | null,
+    rou_description: string | null,
+    rou_bkcolor: string | null,
+    rou_frcolor: string | null,
+    rou_active: boolean | null,
     rou_createdat: Date | null,
     rou_updatedat: Date | null
 }
