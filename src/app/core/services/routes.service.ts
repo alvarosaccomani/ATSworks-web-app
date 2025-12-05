b21f1227ec38eb96b5e7055f2fd10a794ff74d1d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-05 17:39:22 -0300

    add method getRoutes to routes service

--- src/app/core/services/routes.service.ts
+++ src/app/core/services/routes.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { RouteResults } from '../interfaces/route';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoutesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  public getRoutes(cmp_uuid: string, filter?: string, page?: number, perPage?: number): Observable<RouteResults> {
+      let headers = new HttpHeaders().set('content-type','application/json');
+  
+      if(page && perPage) {
+        filter = `${filter}/${page}/${perPage}`;
+      }
+  
+      return this._http.get<RouteResults>(environment.apiUrl + 'routes/' + cmp_uuid + '/' + filter, {headers:headers})
+    }
 }
