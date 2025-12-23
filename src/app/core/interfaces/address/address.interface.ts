a72c9aab9286797357c3e6bc37a6e57927fcf8d9
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-23 10:09:06 -0300

    i add the subp_uuid and subp properties to the address interface

--- src/app/core/interfaces/address/address.interface.ts
+++ src/app/core/interfaces/address/address.interface.ts
@@ -1,4 +1,5 @@
 import { CustomerInterface } from "../customer"
+import { SubscriptionPlanInterface } from "../subscription-plan"
 
 export interface AddressInterface {
     cmp_uuid: string | null,
@@ -10,6 +11,8 @@ export interface AddressInterface {
     adr_province: string | null,
     adr_postalcode: string | null,
     adr_dimensions: string | null,
+    subp_uuid: string | null,
+    subp: SubscriptionPlanInterface | null,
     adr_createdat: Date | null,
     adr_updatedat: Date | null
 }
