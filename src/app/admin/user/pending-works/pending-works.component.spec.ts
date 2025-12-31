4211661c34b7600d334049dfedee8ffd59f1e399
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-31 08:52:40 -0300

    i add pending-works component to project

---
+++ src/app/admin/user/pending-works/pending-works.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PendingWorksComponent } from './pending-works.component';
+
+describe('PendingWorksComponent', () => {
+  let component: PendingWorksComponent;
+  let fixture: ComponentFixture<PendingWorksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PendingWorksComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PendingWorksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/admin/user/pending-works/pending-works.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-works',
+  imports: [],
+  templateUrl: './pending-works.component.html',
+  styleUrl: './pending-works.component.scss'
+})
+export class PendingWorksComponent {
+
+}
