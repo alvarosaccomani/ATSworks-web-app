ba235830b2c642f014d232467d1cf5270e3cd30a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-06 15:35:16 -0300

    i add methods getRoutes, filter and clearSearch to pending-works component

--- src/app/admin/user/pending-works/pending-works.component.ts
+++ src/app/admin/user/pending-works/pending-works.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { PageNavTabsComponent } from '../../../shared/components/page-nav-tabs/page-nav-tabs.component';
+import { RoutesService } from '../../../core/services/routes.service';
 import { WorksService } from '../../../core/services/works.service';
+import { RouteInterface } from '../../../core/interfaces/route';
 import { WorkInterface, WorkResults } from '../../../core/interfaces/work';
 import { SessionService } from '../../../core/services/session.service';
 import { MessageService } from '../../../core/services/message.service';
@@ -14,9 +19,12 @@ import { PaginationComponent } from '../../../shared/components/pagination/pagin
 @Component({
   selector: 'app-pending-works',
   imports: [
+    FormsModule,
     AsyncPipe,
     DatePipe,
     RouterLink,
+    NzIconModule,
+    NzSelectModule,
     HeaderComponent,
     PageNavTabsComponent,
     PaginationComponent
@@ -33,11 +41,14 @@ export class PendingWorksComponent {
 
   // Variables para filtros
   public searchWorkState: string = "bcaa7b3b-cdbf-4b02-8a91-78a67b5aa823";
+  public searchRoute: string = "Lunes";
   public fieldSortValue: string = "wrk_workdate";
   public sortValue: string = "ASC";
 
   private cmp_uuid!: string;
   public works$!: Observable<WorkResults>;
+  public routes: RouteInterface[] = [];
+  public isLoadingRoutes: boolean = false;
   public headerConfig: any = {
     title: "LISTA DE TRABAJOS",
     description: "Listado de Trabajos.",
@@ -64,20 +75,52 @@ export class PendingWorksComponent {
   constructor(
     private _sessionService: SessionService,
     private _messageService: MessageService,
+    private _routesService: RoutesService,
     private _worksService: WorksService,
     private _sharedDataService: SharedDataService
   ) { }
 
   ngOnInit(): void {
     this.cmp_uuid = this._sessionService.getCompany().cmp_uuid;
+    this.getRoutes(this.cmp_uuid!);
 
-    this.works$ = this._worksService.getPendingWorks(this.cmp_uuid, this.searchWorkState, this.page, this.perPage, this.fieldSortValue, this.sortValue);
+    this.works$ = this._worksService.getPendingWorks(this.cmp_uuid, this.searchWorkState, this.searchRoute, this.page, this.perPage, this.fieldSortValue, this.sortValue);
     this._sharedDataService.selectedCompany$.subscribe((company) => {
       if (company) {
         console.info(company);
-        this.works$ = this._worksService.getPendingWorks(company.cmp_uuid, this.searchWorkState, this.page, this.perPage, this.fieldSortValue, this.sortValue);
+        this.works$ = this._worksService.getPendingWorks(company.cmp_uuid, this.searchWorkState, this.searchRoute, this.page, this.perPage, this.fieldSortValue, this.sortValue);
       }
     });
+  }
+
+  private getRoutes(cmp_uuid: string) {
+    this._routesService.getRoutes(cmp_uuid).subscribe(
+      (response: any) => {
+        if(response.success) {
+          console.info(response.data);
+          this.isLoadingRoutes = false;
+          this.routes = response.data;
+        } else {
+          //this.status = 'error'
+        }
+      },
+      (error: any) => {
+        let errorMessage = <any>error;
+        console.log(errorMessage);
+
+        if(errorMessage != null) {
+          //this.status = 'error'
+        }
+      }
+    )
+  }
+
+  public filter(): void {
+    this.works$ = this._worksService.getPendingWorks(this.cmp_uuid, this.searchWorkState, this.searchRoute, this.page, this.perPage, this.fieldSortValue, this.sortValue);
+  }
+
+  public clearSearch(): void {
+    this.searchRoute = "Lunes";
   }
 
   public deleteWork(work: WorkInterface) {
@@ -97,7 +140,7 @@ export class PendingWorksComponent {
             .subscribe(
               response => {
                 console.info(response);
-                this.works$ = this._worksService.getPendingWorks(this.cmp_uuid, this.searchWorkState, this.page, this.perPage, this.fieldSortValue, this.sortValue);
+                this.works$ = this._worksService.getPendingWorks(this.cmp_uuid, this.searchWorkState, this.searchRoute, this.page, this.perPage, this.fieldSortValue, this.sortValue);
               },
               error => {
                 console.log(<any>error);
@@ -110,6 +153,6 @@ export class PendingWorksComponent {
 
   public goToPage(page: number): void {
     this.page = page;
-    this.works$ = this._worksService.getPendingWorks(this.cmp_uuid, this.searchWorkState, page, this.perPage, this.fieldSortValue, this.sortValue);
+    this.works$ = this._worksService.getPendingWorks(this.cmp_uuid, this.searchWorkState, this.searchRoute, page, this.perPage, this.fieldSortValue, this.sortValue);
   }
 }
