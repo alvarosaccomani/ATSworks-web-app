8e2bf76eecc0bf8bf7507fbd149e5f236e763065
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-06 15:41:51 -0300

    i add work-schedule component to project

---
+++ src/app/admin/user/work-schedule/work-schedule.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-schedule',
+  imports: [],
+  templateUrl: './work-schedule.component.html',
+  styleUrl: './work-schedule.component.scss'
+})
+export class WorkScheduleComponent {
+
+}
