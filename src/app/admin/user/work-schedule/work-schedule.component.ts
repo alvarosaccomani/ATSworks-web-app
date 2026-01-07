d1f669f39bf94d314fdd41fe92ef0c31257dc41f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-07 15:38:20 -0300

    i add methods getWorkStates, filter and clearSearch to work-schedule component

--- src/app/admin/user/work-schedule/work-schedule.component.ts
+++ src/app/admin/user/work-schedule/work-schedule.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { NzCalendarModule } from 'ng-zorro-antd/calendar';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { PageNavTabsComponent } from '../../../shared/components/page-nav-tabs/page-nav-tabs.component';
+import { WorkStateInterface } from '../../../core/interfaces/work-state';
+import { WorkStatesService } from '../../../core/services/work-states.service';
 import { WorksService } from '../../../core/services/works.service';
 import { WorkResults } from '../../../core/interfaces/work';
 import { SessionService } from '../../../core/services/session.service';
@@ -12,6 +17,9 @@ import { SharedDataService } from '../../../core/services/shared-data.service';
 @Component({
   selector: 'app-work-schedule',
   imports: [
+    FormsModule,
+    NzIconModule,
+    NzSelectModule,
     NzBadgeModule,
     NzCalendarModule,
     HeaderComponent,
@@ -32,11 +40,13 @@ export class WorkScheduleComponent {
 
   private cmp_uuid!: string;
   public worksScheduler: WorkResults[] = [];
+  public workStates: WorkStateInterface[] = [];
+  public isLoadingWorkStates: boolean = false;
   public eventsByDate: { [key: string]: string[] } = {}; // clave = 'YYYY-MM-DD', valor = lista de clientes
   public monthCounts: { [key: string]: number } = {}; // clave: 'YYYY-MM', valor: cantidad
   public headerConfig: any = {
     title: "CALENDARIO DE TRABAJOS",
-    description: "Listado de Trabajos.",
+    description: "Calendario de Trabajos.",
     icon: "fas fa-calendar fa-fw"
   }
   public dataTabs: any = [
@@ -64,12 +74,14 @@ export class WorkScheduleComponent {
 
   constructor(
     private _sessionService: SessionService,
+    private _workStatesService: WorkStatesService,
     private _worksService: WorksService,
     private _sharedDataService: SharedDataService
   ) { }
 
   ngOnInit(): void {
     this.cmp_uuid = this._sessionService.getCompany().cmp_uuid;
+    this.getWorkStates(this.cmp_uuid!);
 
     this.getWorkScheduler(this.cmp_uuid, this.searchDateFrom, this.searchDateTo, this.searchWorkState, this.searchRoute, this.fieldSortValue, this.sortValue);
     this._sharedDataService.selectedCompany$.subscribe((company) => {
@@ -78,6 +90,37 @@ export class WorkScheduleComponent {
         this.getWorkScheduler(company.cmp_uuid, this.searchDateFrom, this.searchDateTo, this.searchWorkState, this.searchRoute, this.fieldSortValue, this.sortValue);
       }
     });
+  }
+
+  private getWorkStates(cmp_uuid: string) {
+    this._workStatesService.getWorkStates(cmp_uuid).subscribe(
+      (response: any) => {
+        if(response.success) {
+          console.info(response.data);
+          this.isLoadingWorkStates = false;
+          this.workStates = response.data;
+        } else {
+          //this.status = 'error'
+        }
+      },
+      (error: any) => {
+        let errorMessage = <any>error;
+        console.log(errorMessage);
+
+        if(errorMessage != null) {
+          //this.status = 'error'
+        }
+      }
+    )
+  }
+
+  public filter(): void {
+    this.getWorkScheduler(this.cmp_uuid, this.searchDateFrom, this.searchDateTo, this.searchWorkState, this.searchRoute, this.fieldSortValue, this.sortValue);
+  }
+
+  public clearSearch(): void {
+    this.searchWorkState = "bcaa7b3b-cdbf-4b02-8a91-78a67b5aa823",
+    this.searchRoute = "Lunes";
   }
 
   private getWorkScheduler(cmp_uuid: string, wrk_dateFrom: string, wrk_dateTo: string, wrks_uuid: string, wrk_route: string, field_order: string, wrk_order: string) {
