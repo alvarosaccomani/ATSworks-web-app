d0c024dab8004729d264a6f31ea7141b9290e06c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-07 14:59:25 -0300

    add method getWorkStates to work-states service

--- src/app/core/services/work-states.service.ts
+++ src/app/core/services/work-states.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { WorkStateResults } from '../interfaces/work-state'
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkStatesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  public getWorkStates(cmp_uuid: string, filter?: string, page?: number, perPage?: number): Observable<WorkStateResults> {
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    if(page && perPage) {
+      filter = `${filter}/${page}/${perPage}`;
+    }
+
+    return this._http.get<WorkStateResults>(environment.apiUrl + 'work-states/' + cmp_uuid + '/' + filter, {headers:headers})
+  }
+
 }
