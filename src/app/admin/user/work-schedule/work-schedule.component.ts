e37f0744f556ea9fa3ac8603f7adb8a1c40cbd93
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-07 14:05:24 -0300

    i add methods getWorkScheduler, buildEventsByDate, getCustomersByDate, buildMonthCounts and getMonthData to work-schedule component

--- src/app/admin/user/work-schedule/work-schedule.component.ts
+++ src/app/admin/user/work-schedule/work-schedule.component.ts
@@ -1,11 +1,148 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzCalendarModule } from 'ng-zorro-antd/calendar';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { PageNavTabsComponent } from '../../../shared/components/page-nav-tabs/page-nav-tabs.component';
+import { WorksService } from '../../../core/services/works.service';
+import { WorkResults } from '../../../core/interfaces/work';
+import { SessionService } from '../../../core/services/session.service';
+import { SharedDataService } from '../../../core/services/shared-data.service';
 
 @Component({
   selector: 'app-work-schedule',
-  imports: [],
+  imports: [
+    NzBadgeModule,
+    NzCalendarModule,
+    HeaderComponent,
+    PageNavTabsComponent
+  ],
   templateUrl: './work-schedule.component.html',
   styleUrl: './work-schedule.component.scss'
 })
 export class WorkScheduleComponent {
 
+  // Variables para filtros
+  public searchDateFrom: string = "";
+  public searchDateTo: string = "";
+  public searchWorkState: string = "bcaa7b3b-cdbf-4b02-8a91-78a67b5aa823";
+  public searchRoute: string = "Lunes";
+  public fieldSortValue: string = "wrk_workdate";
+  public sortValue: string = "ASC";
+
+  private cmp_uuid!: string;
+  public worksScheduler: WorkResults[] = [];
+  public eventsByDate: { [key: string]: string[] } = {}; // clave = 'YYYY-MM-DD', valor = lista de clientes
+  public monthCounts: { [key: string]: number } = {}; // clave: 'YYYY-MM', valor: cantidad
+  public headerConfig: any = {
+    title: "CALENDARIO DE TRABAJOS",
+    description: "Listado de Trabajos.",
+    icon: "fas fa-calendar fa-fw"
+  }
+  public dataTabs: any = [
+    {
+      url: ['/admin/user/work','new'],
+      icon: "fas fa-plus fa-fw",
+      title: "NUEVO TRABAJO"
+    },
+    {
+       url: ['/admin/user/works'],
+       icon: "fas fa-clipboard-list fa-fw",
+       title: "LISTA DE TRABAJOS"
+    },
+    {
+       url: ['/admin/user/pending-works'],
+       icon: "fas fa-hand-holding-usd fa-fw",
+       title: "TRABAJOS PENDIENTES"
+    },
+    {
+       url: ['/admin/user/work-schedule'],
+       icon: "fas fa-calendar fa-fw",
+       title: "CALENDARIO DE TRABAJOS"
+    }
+  ]
+
+  constructor(
+    private _sessionService: SessionService,
+    private _worksService: WorksService,
+    private _sharedDataService: SharedDataService
+  ) { }
+
+  ngOnInit(): void {
+    this.cmp_uuid = this._sessionService.getCompany().cmp_uuid;
+
+    this.getWorkScheduler(this.cmp_uuid, this.searchDateFrom, this.searchDateTo, this.searchWorkState, this.searchRoute, this.fieldSortValue, this.sortValue);
+    this._sharedDataService.selectedCompany$.subscribe((company) => {
+      if (company) {
+        console.info(company);
+        this.getWorkScheduler(company.cmp_uuid, this.searchDateFrom, this.searchDateTo, this.searchWorkState, this.searchRoute, this.fieldSortValue, this.sortValue);
+      }
+    });
+  }
+
+  private getWorkScheduler(cmp_uuid: string, wrk_dateFrom: string, wrk_dateTo: string, wrks_uuid: string, wrk_route: string, field_order: string, wrk_order: string) {
+    this._worksService.getWorkScheduler(cmp_uuid, wrk_dateFrom, wrk_dateTo, wrks_uuid, wrk_route, field_order, wrk_order).subscribe(
+      (response: any) => {
+        if(response.success) {
+          console.info(response.data);
+          this.worksScheduler = response.data;
+          this.buildEventsByDate(response.data);
+          this.buildMonthCounts(response.data);
+        } else {
+          //this.status = 'error'
+        }
+      },
+      (error: any) => {
+        let errorMessage = <any>error;
+        console.log(errorMessage);
+
+        if(errorMessage != null) {
+          //this.status = 'error'
+        }
+      }
+    )
+  }
+
+  private buildEventsByDate(workItems: any) {
+    this.eventsByDate = {};
+
+    for (const item of workItems) {
+      const date = new Date(item.wrk_workdate);
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // meses son 0-indexados
+      const day = String(date.getDate()).padStart(2, '0');
+      const dateKey = `${year}-${month}-${day}`;
+
+      if (!this.eventsByDate[dateKey]) {
+        this.eventsByDate[dateKey] = [];
+      }
+
+      this.eventsByDate[dateKey].push(item.wrk_customer);
+    }
+  }
+
+  public getCustomersByDate(date: Date): string[] {
+    const key = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+    return this.eventsByDate[key] || [];
+  }
+
+  private buildMonthCounts(workItems: any) {
+    this.monthCounts = {};
+
+    for (const item of workItems) {
+      const date = new Date(item.wrk_workdate);
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // meses 01–12
+      const key = `${year}-${month}`;
+
+      this.monthCounts[key] = (this.monthCounts[key] || 0) + 1;
+    }
+  }
+
+  public getMonthData(date: Date): number | null {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const key = `${year}-${month}`;
+    return this.monthCounts[key] ?? null;
+  }
 }
