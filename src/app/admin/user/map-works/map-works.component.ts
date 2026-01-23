9b92d8c2e66a43c81d7f48ed6439196c5cde598b
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-23 14:07:31 -0300

    i add map-works component to project

---
+++ src/app/admin/user/map-works/map-works.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-works',
+  imports: [],
+  templateUrl: './map-works.component.html',
+  styleUrl: './map-works.component.scss'
+})
+export class MapWorksComponent {
+
+}
