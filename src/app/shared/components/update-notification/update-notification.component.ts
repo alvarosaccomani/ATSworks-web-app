d02e6b34740fd39b8af4f37e3923cb91a7b653d2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-30 19:19:25 -0300

    i add update-notification component to project

---
+++ src/app/shared/components/update-notification/update-notification.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-notification',
+  imports: [],
+  templateUrl: './update-notification.component.html',
+  styleUrl: './update-notification.component.scss'
+})
+export class UpdateNotificationComponent {
+
+}
