0a9fa92f18e7fb00ab8faf2e4a1568b8c868bf68
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-30 19:04:38 -0300

    i add method search to geocoder service

--- src/app/core/services/geocoder.service.ts
+++ src/app/core/services/geocoder.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface GeocodingResult {
+  place_id: number;
+  display_name: string;
+  lat: string;
+  lon: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeocoderService {
 
-  constructor() { }
+  private baseUrl = 'https://nominatim.openstreetmap.org/search';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  public search(query: string): Observable<GeocodingResult[]> {
+    const params = {
+      q: query,
+      format: 'json',
+      addressdetails: '1',
+      limit: '5'
+    };
+
+    return this.http.get<GeocodingResult[]>(this.baseUrl, { params }).pipe(
+      map(results => results.map(r => ({
+        ...r,
+        lat: r.lat,
+        lon: r.lon
+      })))
+    );
+  }
 }
