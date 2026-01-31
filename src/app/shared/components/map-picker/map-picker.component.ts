054610f3445f32aa471f73743938aeeca35c7bfa
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-31 11:34:36 -0300

    i add map-picker component to project

---
+++ src/app/shared/components/map-picker/map-picker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-picker',
+  imports: [],
+  templateUrl: './map-picker.component.html',
+  styleUrl: './map-picker.component.scss'
+})
+export class MapPickerComponent {
+
+}
