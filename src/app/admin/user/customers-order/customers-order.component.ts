f180a197b4ef0ee84d7c5c376a976c55c21204bc
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-05 16:21:59 -0300

    i add customers-order component to project

---
+++ src/app/admin/user/customers-order/customers-order.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-order',
+  imports: [],
+  templateUrl: './customers-order.component.html',
+  styleUrl: './customers-order.component.scss'
+})
+export class CustomersOrderComponent {
+
+}
