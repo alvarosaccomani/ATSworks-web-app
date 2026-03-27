e6b2a4bdb1e3b5d9c869a80200c1a7f387892a3e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-27 16:24:28 -0300

    i rename parameter cus_order by cus_orderby

--- src/app/core/services/customers.service.ts
+++ src/app/core/services/customers.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { CustomerResults  } from '../interfaces/customer';
+import { CustomerResults } from '../interfaces/customer';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class CustomersService {
     private _http: HttpClient
   ) { }
 
-  public getCustomers(cmp_uuid: string, cus_fullname?: string, cus_email?: string, page?: number, perPage?: number, field_order?: string, cus_order?: string): Observable<CustomerResults> {
+  public getCustomers(cmp_uuid: string, cus_fullname?: string, cus_email?: string, page?: number, perPage?: number, field_order?: string, cus_orderby?: string): Observable<CustomerResults> {
     const headers = new HttpHeaders().set('content-type', 'application/json');
 
     let params = new HttpParams();
@@ -34,40 +34,40 @@ export class CustomersService {
       params = params.set('perPage', perPage.toString());
     }
 
-    if(field_order) {
+    if (field_order) {
       params = params.set('field_order', field_order);
     }
 
-    if(cus_order) {
-      params = params.set('cus_order', cus_order);
+    if (cus_orderby) {
+      params = params.set('cus_orderby', cus_orderby);
     }
 
     return this._http.get<CustomerResults>(`${environment.apiUrl}customers/${cmp_uuid}`, { headers, params });
   }
 
   public getCustomerById(cmp_uuid: string, cus_uuid?: string): Observable<any> {
-    let headers = new HttpHeaders().set('content-type','application/json');
+    let headers = new HttpHeaders().set('content-type', 'application/json');
 
-    return this._http.get(environment.apiUrl + 'customer/' + cmp_uuid + '/' + cus_uuid, {headers:headers});
+    return this._http.get(environment.apiUrl + 'customer/' + cmp_uuid + '/' + cus_uuid, { headers: headers });
   }
 
   public saveCustomer(customer: any): Observable<any> {
     let params = JSON.stringify(customer);
-    let headers = new HttpHeaders().set('content-type','application/json');
+    let headers = new HttpHeaders().set('content-type', 'application/json');
 
-    return this._http.post(environment.apiUrl + 'customer', params, {headers:headers});
+    return this._http.post(environment.apiUrl + 'customer', params, { headers: headers });
   }
 
   public updateCustomer(customer: any): Observable<any> {
     let params = JSON.stringify(customer);
-    let headers = new HttpHeaders().set('content-type','application/json');
+    let headers = new HttpHeaders().set('content-type', 'application/json');
 
-    return this._http.put(environment.apiUrl + 'customer/' + customer.cmp_uuid + '/' + customer.cus_uuid, params, {headers:headers});
+    return this._http.put(environment.apiUrl + 'customer/' + customer.cmp_uuid + '/' + customer.cus_uuid, params, { headers: headers });
   }
 
   public deleteCustomer(cmp_uuid: string, cus_uuid: string): Observable<any> {
-    let headers = new HttpHeaders().set('content-type','application/json');
+    let headers = new HttpHeaders().set('content-type', 'application/json');
 
-    return this._http.delete(environment.apiUrl + 'customer/' + cmp_uuid + '/' + cus_uuid, {headers:headers});
+    return this._http.delete(environment.apiUrl + 'customer/' + cmp_uuid + '/' + cus_uuid, { headers: headers });
   }
 }
