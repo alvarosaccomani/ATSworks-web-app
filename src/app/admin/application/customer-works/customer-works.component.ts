8764fc601971adfb89c343fe473ab863207e4bd0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-31 10:11:12 -0300

    i add customer-works component to project

---
+++ src/app/admin/application/customer-works/customer-works.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-works',
+  imports: [],
+  templateUrl: './customer-works.component.html',
+  styleUrl: './customer-works.component.scss'
+})
+export class CustomerWorksComponent {
+
+}
