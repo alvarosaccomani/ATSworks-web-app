59397c7e4fde58158726c958926daaa5138f06c6
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-08 11:02:52 -0300

    i add the file customer.routes.ts to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
         path: 'admin',
         loadChildren: () => import('./admin/admin.routes').then(m => m.ADMIN_ROUTES)
     },
+    { 
+        path: 'customer',
+        loadChildren: () => import('./customer/customer.routes').then(m => m.CUSTOMER_ROUTES)
+    },
     { path: '**', redirectTo: 'auth/login'}
 ];
