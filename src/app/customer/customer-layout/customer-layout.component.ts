57ed0bd6efbc8fae3e305a628f66158a607df167
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-08 13:56:45 -0300

    add router-outlet to customer-layout component

--- src/app/customer/customer-layout/customer-layout.component.ts
+++ src/app/customer/customer-layout/customer-layout.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
+import { UserInterface } from '../../core/interfaces/user';
+import { UsersService } from '../../core/services/users.service';
+
 @Component({
   selector: 'app-customer-layout',
-  imports: [],
+  imports: [
+    RouterModule,
+    NavBarComponent
+  ],
   templateUrl: './customer-layout.component.html',
   styleUrl: './customer-layout.component.scss',
 })
 export class CustomerLayoutComponent {
+
+  public identity!: UserInterface;
+
+  constructor(
+    private _usersService: UsersService
+  ) { }
+
+  ngOnInit(): void {
+    this.identity = this._usersService.getIdentity();
+  }
 
 }
