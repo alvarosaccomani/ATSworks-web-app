3659f80086d8b1db267eefa72cedbe9c137bb27a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-08 13:57:46 -0300

    i add customer-works component in customer folder to project

---
+++ src/app/customer/customer-works/customer-works.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomerWorksComponent } from './customer-works.component';
+
+describe('CustomerWorksComponent', () => {
+  let component: CustomerWorksComponent;
+  let fixture: ComponentFixture<CustomerWorksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CustomerWorksComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CustomerWorksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
