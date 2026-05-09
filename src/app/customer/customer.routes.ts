13bdcad061e0739ce35dd373a4beca4a491ff2a8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-09 11:02:32 -0300

    i add the customer route in customer.routes.ts

--- src/app/customer/customer.routes.ts
+++ src/app/customer/customer.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 
 import { CustomerLayoutComponent } from './customer-layout/customer-layout.component';
 import { CustomerWorksComponent } from './customer-works/customer-works.component';
+import { UserComponent } from './user/user.component';
 
 export const CUSTOMER_ROUTES: Routes = [
     {
         path: '',
         component: CustomerLayoutComponent,
         children: [
-            { path: 'customer-works', component: CustomerWorksComponent }
+            { path: 'customer-works', component: CustomerWorksComponent },
+            { path: 'user', component: UserComponent },
         ]
     }
 ];
